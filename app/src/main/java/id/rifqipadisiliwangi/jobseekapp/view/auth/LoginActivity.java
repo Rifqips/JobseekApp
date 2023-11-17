888903dc1d5b79f352d9f395bcbf00935aa673c6
Rifqi Padi Siliwangi <79899198+Rifqips@users.noreply.github.com>
2023-11-18 04:50:14 +0700

    refactor
    add login activity

--- app/src/main/java/id/rifqipadisiliwangi/jobseekapp/view/auth/LoginActivity.java
+++ app/src/main/java/id/rifqipadisiliwangi/jobseekapp/view/auth/LoginActivity.java
@@ -1,4 +1,4 @@
-package id.rifqipadisiliwangi.jobseekapp.view.onboarding;
+package id.rifqipadisiliwangi.jobseekapp.view.auth;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,25 +8,26 @@ import android.view.View;
 
 import id.rifqipadisiliwangi.jobseekapp.R;
 import id.rifqipadisiliwangi.jobseekapp.databinding.ActivityDashboardBinding;
-import id.rifqipadisiliwangi.jobseekapp.databinding.ActivityOnboardingBinding;
+import id.rifqipadisiliwangi.jobseekapp.databinding.ActivityLoginBinding;
+import id.rifqipadisiliwangi.jobseekapp.view.dashboard.DashboardActivity;
 import id.rifqipadisiliwangi.jobseekapp.view.detail.DetailActivity;
+import id.rifqipadisiliwangi.jobseekapp.view.onboarding.OnboardingActivity;
 
-public class OnboardingActivity extends AppCompatActivity {
-
-    ActivityOnboardingBinding binding;
+public class LoginActivity extends AppCompatActivity {
+    ActivityLoginBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityOnboardingBinding.inflate(getLayoutInflater());
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
 
-
         binding.btnLogin.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                Intent intent = new Intent(OnboardingActivity.this, DetailActivity.class);
+                Intent intent = new Intent(LoginActivity.this, DashboardActivity.class);
                 startActivity(intent);
+                finish();
             }
         });
     }
